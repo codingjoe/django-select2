005efd8a1e6fb612196e99bf552d490c4037abcd
jasmanysanchez97 <106758719+jasmanysanchez97@users.noreply.github.com>
2022-11-30 08:43:28 -0500

    Update django_select2.js
    "formset:added" EventListener for InlineForms and discard Parent Element with class name: ".empty-form"

--- django_select2/static/django_select2/django_select2.js
+++ django_select2/static/django_select2/django_select2.js
@@ -1,75 +1,100 @@
 /* global define, jQuery */
 (function (factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['jquery'], factory)
-  } else if (typeof module === 'object' && module.exports) {
-    module.exports = factory(require('jquery'))
-  } else {
-    // Browser globals
-    factory(jQuery || window.django.jQuery)
-  }
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory)
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require('jquery'))
+    } else {
+        // Browser globals
+        factory(jQuery || window.django.jQuery)
+    }
 }(function ($) {
-  'use strict'
-  var init = function ($element, options) {
-    $element.select2(options)
-  }
+    'use strict'
+    var init = function ($element, options) {
+        $element.select2(options)
+    }
 
-  var initHeavy = function ($element, options) {
-    var settings = $.extend({
-      ajax: {
-        data: function (params) {
-          var result = {
-            term: params.term,
-            page: params.page,
-            field_id: $element.data('field_id')
-          }
+    var initHeavy = function ($element, options) {
+        var settings = $.extend({
+            ajax: {
+                data: function (params) {
+                    var result = {
+                        term: params.term,
+                        page: params.page,
+                        field_id: $element.data('field_id')
+                    }
 
-          var dependentFields = $element.data('select2-dependent-fields')
-          if (dependentFields) {
-            dependentFields = dependentFields.trim().split(/\s+/)
-            $.each(dependentFields, function (i, dependentField) {
-              result[dependentField] = $('[name=' + dependentField + ']', $element.closest('form')).val()
-            })
-          }
+                    var dependentFields = $element.data('select2-dependent-fields')
+                    if (dependentFields) {
+                        dependentFields = dependentFields.trim().split(/\s+/)
+                        $.each(dependentFields, function (i, dependentField) {
+                            result[dependentField] = $('[name=' + dependentField + ']', $element.closest('form')).val()
+                        })
+                    }
 
-          return result
-        },
-        processResults: function (data, page) {
-          return {
-            results: data.results,
-            pagination: {
-              more: data.more
+                    return result
+                },
+                processResults: function (data, page) {
+                    return {
+                        results: data.results,
+                        pagination: {
+                            more: data.more
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    }, options)
+        }, options)
 
-    $element.select2(settings)
-  }
+        $element.select2(settings)
+    }
 
-  $.fn.djangoSelect2 = function (options) {
-    var settings = $.extend({}, options)
-    $.each(this, function (i, element) {
-      var $element = $(element)
-      if ($element.hasClass('django-select2-heavy')) {
-        initHeavy($element, settings)
-      } else {
-        init($element, settings)
-      }
-      $element.on('select2:select', function (e) {
-        var name = $(e.currentTarget).attr('name')
-        $('[data-select2-dependent-fields~=' + name + ']').each(function () {
-          $(this).val('').trigger('change')
+    $.fn.djangoSelect2 = function (options) {
+        var settings = $.extend({}, options)
+        $.each(this, function (i, element) {
+            var $element = $(element)
+            if ($element.hasClass('django-select2-heavy')) {
+                initHeavy($element, settings)
+            } else {
+                init($element, settings)
+            }
+            $element.on('select2:select', function (e) {
+                var name = $(e.currentTarget).attr('name')
+                $('[data-select2-dependent-fields~=' + name + ']').each(function () {
+                    $(this).val('').trigger('change')
+                })
+            })
         })
-      })
+        return this
+    }
+
+    function initDjangoSelect2(element){
+        if(element.classList.contains('django-select2')){
+            $(element).djangoSelect2();
+        }else{
+            element.querySelectorAll('.django-select2').forEach(djSelect => {
+                if (djSelect.closest('.empty-form') === null) {
+                    $(djSelect).djangoSelect2();
+                }
+            });
+        }
+    }
+
+    $(function () {
+        document.querySelectorAll('.django-select2').forEach(element => {
+            if (element.closest('.empty-form') === null) {
+                $(element).djangoSelect2();
+            }
+        });
+        $(document).on('formset:added', (event, $row, formsetName) => {
+            if (event.detail && event.detail.formsetName) {
+                // Django >= 4.1
+                initDjangoSelect2(event.target);
+            } else {
+                // Django < 4.1, use $row
+                initDjangoSelect2($row.get(0));
+            }
+        });
     })
-    return this
-  }
 
-  $(function () {
-    $('.django-select2').djangoSelect2()
-  })
-
-  return $.fn.djangoSelect2
+    return $.fn.djangoSelect2
 }))
