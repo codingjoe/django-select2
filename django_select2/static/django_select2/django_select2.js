8a18a29a3e3fd185cafc9f760b05a9a45e9e045d
Ombental <30341654+Ombental@users.noreply.github.com>
2023-08-26 12:41:31 +0300

    try patching django select2 dependent fields for inlines

--- django_select2/static/django_select2/django_select2.js
+++ django_select2/static/django_select2/django_select2.js
@@ -25,10 +25,19 @@
           }
 
           var dependentFields = $element.data('select2-dependent-fields')
+          var dependentFields = $element.data('select2-dependent-fields')
           if (dependentFields) {
             dependentFields = dependentFields.trim().split(/\s+/)
             $.each(dependentFields, function (i, dependentField) {
-              result[dependentField] = $('[name=' + dependentField + ']', $element.closest('form')).val()
+              var formValue = $('[name=' + dependentField + ']', $element.closest('form')).val();
+              // This is for inlines, I checked this for a specific case
+              if (formValue === null || formValue === undefined) {
+                  var newFieldName = $element[0].name.split('-', 2).join('-') + '-' + dependentField;
+                  var formsetValue = $('[name=' + newFieldName + ']', $element.closest('.form-row')).val();
+                  result[dependentField] = formsetValue;
+              } else {
+                  result[dependentField] = formValue;
+              }
             })
           }
 
